5ee28375d7b0dfb437dbacf90a1985e254deca5c
Sajan Singh Shergill <ss865268@gmail.com>
2025-01-07 17:10:44 -0500

    added comments

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,29 @@
 import sys
-from src.logger import logging
+from logger import logging
 
-def error_message_details(error, error_detail:sys):
-    _, _, exc_tb=error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
+def error_message_details(error, error_detail:sys): #error_detail will be there in sys
+    _, _, exc_tb=error_detail.exc_info()  #exc_tb will provide the information about why the exception has occured, which file and which line number the exception has occured, etc...
+    file_name = exc_tb.tb_frame.f_code.co_filename #tb_frame, f_code and co_filename are properties. Visti custom exception handing documentation on web
     error_message = "Error occured in Python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error))
+        file_name, exc_tb.tb_lineno, str(error)) 
     
     return error_message
     
-    
+
+#Whenever cutomeException is raised, first of all it inherits the parent exception, 
+# whatever error_message is coming from the function 'error_message_details', 
+# that particular message willl come over here and we will intialize it to customException variable
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
+        super().__init__(error_message) #As we are inherting from the exception
         self.error_message= error_message_details(error_message, error_detail = error_detail)
-    
     
     def __str__(self):
         return self.error_message
+    
+if __name__ =="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("dIVIDE BY 0")
+        raise CustomException(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,14 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok= True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" #text file where these are the naming convention using which file will be named
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE) #initially the "logs" mean logs/then the naming conventions
+os.makedirs(logs_path, exist_ok= True) #Even though there is a file or folder, keep on appending the files whenever we want to create the file
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE) 
 
+# Wherever logging.INFO or 'import loggin' or write out any print message then it is going to use the below kind of basicConfig:
+# 1. Create this file path in the format
 logging.basicConfig(
     filename=LOG_FILE_PATH, 
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
